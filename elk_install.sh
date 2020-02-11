726f7cf48cbb33a4b7b050872bb4fd38b90aecc9
Vivek Shwarup <32225812+vivekshwarup@users.noreply.github.com>
2020-02-11 17:06:23 +0530

    Create elk_install.sh

---
+++ elk_install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+add-apt-repository ppa:openjdk-r/ppa
+apt update -y
+apt install openjdk-11-jdk -y
+echo "JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/" >> /etc/environment
+source /etc/environment
+apt install -y https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-oss-7.4.2-amd64.deb https://artifacts.elastic.co/downloads/kibana/kibana-oss-7.4.2-amd64.deb https://artifacts.elastic.co/downloads/logstash/logstash-oss-7.4.2.deb https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-oss-7.4.2-amd64.deb
